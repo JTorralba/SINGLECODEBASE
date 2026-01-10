469ea043abd1b908faa5ae9d74c7049b92e7dfcf
Julius Torralba <Julius.Torralba@GMail.com>
2026-01-10 16:21:10 -0700

    FIX_000005 Disable Android scroll overflow animation.

--- SCB/SCB/MainPage.xaml.cs
+++ SCB/SCB/MainPage.xaml.cs
@@ -6,5 +6,15 @@
         {
             InitializeComponent();
         }
+
+        protected override void OnHandlerChanged()
+        {
+            base.OnHandlerChanged();
+#if ANDROID
+            var blazorView = this.blazorWebView;
+            var platformView = (Android.Webkit.WebView)blazorView.Handler.PlatformView;
+            platformView.OverScrollMode = Android.Views.OverScrollMode.Never;
+#endif
+        }
     }
 }
